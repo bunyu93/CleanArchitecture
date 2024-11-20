3486d5dff44198a93376edebeadae3fbb4a9766c
Bunyamin Sakar <bunyamin.sakar@live.nl>
2024-11-20 23:03:04 +0100

    ref: added init migrations required since EF 9

---
+++ src/CleanArchitectureTemplate.Persistence/Migrations/20241120220043_init.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitectureTemplate.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherForecast",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(type: "DATE", nullable: false),
+                    Fahrenheit = table.Column<int>(type: "INTEGER", nullable: false),
+                    Celsius = table.Column<int>(type: "INTEGER", nullable: false),
+                    Summary = table.Column<string>(type: "STRING", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherForecast", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherForecast");
+        }
+    }
+}
